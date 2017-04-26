6d902d09b51abbd4802118d475ebf7a9106c6b90
Evgenia Yavorskaya <night994@gmail.com>
2017-04-26 22:01:47 +0300

    Configure Material UI

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
